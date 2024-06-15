98dfa66b83606942ff97ca41b7c8c147cf7264c2
Yiwen Zhang <yiwenmary@gmail.com>
2024-06-15 09:38:54 -0400

    firebase deploy

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,14 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// exports.sendMessage = functions.firestore
+//   .document("products/{productId}")
+//   .onCreate((event) => {
+//     const docId = event.params.productId;
+//     const name = event.data.data().name;
+//     const productRef = admin.firestore().collection("products").doc(docId);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//     return productRef.update({ message: `Nice ${name}!` });
+//   });
